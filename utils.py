ddbe0185e8954db5c4ee5f003dac571454f4c9d7
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-05-07 17:27:29 -0300

    Implementa verificação de musica existente em playlist

--- utils.py
+++ utils.py
@@ -5,3 +5,20 @@ def obter_nome_playlist_id(sp, playlist_id):
     except Exception as e:
         print(f"❌ Erro ao obter nome da playlist: {e}")
         return "playlist_desconecida"
+    
+def verificar_musica_na_playlist(sp, playlist_id, track_id):
+    try:
+        resultados = sp.playlist_items(playlist_id, fields="items.track.id,next", additional_types=["track"])
+        while resultados:
+            for item in resultados["items"]:
+                track = item["track"]
+                if track and track["id"] == track_id:
+                    return True
+            if resultados.get("next"):
+                resultados = sp.next(resultados)
+            else:
+                break
+        return False
+    except Exception as e:
+        print(f"❌ Erro ao verificar música na playlist: {e}")
+        return False
