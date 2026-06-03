de228ee2e20ff5b852a783cbddbd8cc61bad1590
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-06-02 22:13:21 -0300

    Implementa listagem de playlists existentes

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -23,6 +23,29 @@ def verificar_musica_na_playlist(sp, playlist_id, track_id):
         print(f"❌ Erro ao verificar música na playlist: {e}")
         return False
     
+def listar_playlists(sp):
+    playlists = []
+
+    resultados = sp.current_user_playlists()
+
+    while resultados:
+        for item in resultados["items"]:
+            playlists.append({
+                "id": item["id"],
+                "nome": item["name"],
+                "total_musicas": item["tracks"]["total"]
+            })
+
+        if resultados["next"]:
+            resultados = sp.next(resultados)
+        else:
+            break
+    print(f"✅ Total de playlists encontradas: {len(playlists)}")
+    for p in playlists:
+        print(f"- {p['nome']} (ID: {p['id']}, Total de músicas: {p['total_musicas']})")
+        
+    return playlists
+
 def jaccard_sim(a, b):
     '''Calcula a similaridade de Jaccard entre duas strings.
     A similaridade de Jaccard é definida como o tamanho da interseção dividido pelo tamanho da união dos conjuntos de palavras.
@@ -32,3 +55,9 @@ def jaccard_sim(a, b):
     intersec = a_set.intersection(b_set)
     union = a_set.union(b_set)
     return len(intersec) / len(union) if union else 0
+
+# # teste de ler listar playlists
+# if __name__ == "__main__":
+#     from backend.core import autenticar_spotify as connect
+#     sp = connect.autentica_spotify()
+#     listar_playlists(sp)
