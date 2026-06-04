6c332d48e561209a66ca99f0e969a1172e45ef8f
Marlysson Silva Dantas <marlyssonsd@gmail.com>
2026-06-03 22:42:01 -0300

    Atualiza configurações para permitir tudo ou quase ....

--- config.py
+++ config.py
@@ -4,7 +4,19 @@ ARQUIVO_MUSICAS = "database/musicas_nomes_organizados.json"
 NOME_PLAYLIST = "X-Tudinho"
 
 # Escopo para criar playlists privadas
-SCOPE_PRIVATE = "playlist-modify-private"
+SCOPE_PRIVATE = (
+    "user-library-read "
+    "playlist-read-private "
+    "playlist-read-collaborative "
+    "playlist-modify-private "
+    "playlist-modify-public "
+    "user-read-private "
+    "user-read-email "
+    "user-top-read "
+    "user-read-recently-played "
+    "user-follow-read "
+    "user-follow-modify"
+)
 
 # Nível de log (DEBUG, INFO, WARNING, ERROR, CRITICAL)
 LOG_LEVEL = "INFO"
